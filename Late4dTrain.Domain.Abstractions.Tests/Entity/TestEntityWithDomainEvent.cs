ff6e3702c99205f203b6cfa529079c12e196eee7
Gab Soulavy <1517510+gsoulavy@users.noreply.github.com>
2022-01-24 12:08:45 +0000

    support standard2.0

--- Late4dTrain.Domain.Abstractions.Tests/Entity/TestEntityWithDomainEvent.cs
+++ Late4dTrain.Domain.Abstractions.Tests/Entity/TestEntityWithDomainEvent.cs
@@ -1,19 +1,18 @@
-﻿namespace Late4dTrain.Domain.Abstractions.Tests.Entity;
-
-using System;
-
-using Abstractions;
-
-internal class TestEntityWithDomainEvent
-    : Entity<Guid>.WithDomainEvent<DomainEvent>, IAggregateRoot
+﻿namespace Late4dTrain.Domain.Abstractions.Tests.Entity
 {
-    public TestEntityWithDomainEvent(Guid id, string name)
+    using System;
+
+    internal class TestEntityWithDomainEvent
+        : Entity<Guid>.WithDomainEvent<DomainEvent>, IAggregateRoot
     {
-        (Id, Name) = (id, name);
-        Events.Add(new TestEntityCreated());
+        public TestEntityWithDomainEvent(Guid id, string name)
+        {
+            (Id, Name) = (id, name);
+            Events.Add(new TestEntityCreated());
+        }
+
+        public string Name { get; }
+
+        public sealed override Guid Id { get; protected set; }
     }
-
-    public string Name { get; }
-
-    public sealed override Guid Id { get; protected set; }
 }
